11ff62363800402bd6559b2810c72634820f9d44
AstridGlauser <84193709+AstridGlauser@users.noreply.github.com>
2022-04-08 21:44:38 -0600

    Add files via upload

---
+++ JUnitAssociation.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package javaapplication29;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+
+public class JUnitAssociation {
+     @Test
+    public void test1(){
+    
+Association<String, String> asociac = new Association<String, String>("woman", "mujer");
+assertEquals(asociac.getKey(),"woman");
+assertEquals(asociac.getValue(),"mujer");
+
+ 
+    
+ 
+
+
+
+
+ 
+ 
+
+
+
+
+    }
+
+}
